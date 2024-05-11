e8e2834701908d4a347d767e7f07c3f15c38169e
Sachin Maurya <135147793+smaurya4941@users.noreply.github.com>
2024-05-11 11:47:48 +0000

    commit changes

---
+++ basics-js/datatypes.js
@@ -0,0 +1,18 @@
+//two types
+
+///1 .Primitive datatypes >>>>Call by value hote hai>>>>copy of original data is accessed only
+//THEY are:& types String,NUmber,boolean,null,undefined,Symbol,BigInt
+const score=100; //Number
+const isScore=true;  ///boolean
+let userEmail;  ///undefined
+
+///2.Non Primitive:>>>>Call by reference ::original value is passed
+//Example:::Array,Objects,Functions
+const heroes=["tony","thar","captain"]; //array
+let obj={   ///object
+    name: "sachin",
+    age:21
+};
+
+
+//javascript is dynamically typed language:::NO need to tell data types
